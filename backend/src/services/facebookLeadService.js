01ec4ea2680a810e250c4f7e9b64c51912cf1fd1
Shivam <main.shivamsharma@gmail.com>
2025-05-26 16:17:08 +0530

    another oopsie

--- backend/src/services/facebookLeadService.js
+++ backend/src/services/facebookLeadService.js
@@ -1,4 +1,4 @@
-const saveUser = require("../model/fbModel");
+const { saveUser } = require("../model/fbModel");
 async function getFbUser(userAccessToken, userName) {
   const saveUsers = saveUser(userAccessToken, userName);
   return saveUsers;
