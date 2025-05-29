79dfdb92f6deb5cc78ccc200924762e743c195a7
Shivam <main.shivamsharma@gmail.com>
2025-05-29 15:42:21 +0530

    fix 12

--- backend/src/services/onboardingService.js
+++ backend/src/services/onboardingService.js
@@ -4,6 +4,7 @@ const crypto = require("crypto");
 const { saveOnboardingClient } = require("../model/onboardingClientModel");
 
 const processOnboardingData = async (fields, file) => {
+  console.log(safeFbId);
   if (!file) {
     throw new Error("Image file is required.");
   }
